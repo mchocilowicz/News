1d68c198156ba3cbc38b4fd778952777cfb0836b
Marcin Chocilowicz <marcin.chocilowicz@gmail.com>
2018-12-12 19:03:02 +0100

    Added Api Description

--- backend/src/main/java/com/mrch/backend/controllers/NewsController.java
+++ backend/src/main/java/com/mrch/backend/controllers/NewsController.java
@@ -2,6 +2,7 @@ package com.mrch.backend.controllers;
 
 import com.mrch.backend.models.NewsDTO;
 import com.mrch.backend.services.NewsService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ public class NewsController {
     @Autowired
     NewsService newsService;
 
+    @ApiOperation(value = "Get News by country and category", response = NewsDTO.class)
     @GetMapping("/{country}/{category}")
     public NewsDTO getNews(@PathVariable("country") String country, @PathVariable("category") String category) {
         return newsService.getNewsByCountryCategory(country,category);
